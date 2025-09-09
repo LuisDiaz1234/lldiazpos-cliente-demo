264d4fe386508443f6d5867f8da22b1bb0ceb80c
LuisDiaz1234 <131550504+LuisDiaz1234@users.noreply.github.com>
2025-09-09 12:51:19 -0600

    Create page.tsx

---
+++ apps/web/app/(purchasing)/compras/page.tsx
@@ -0,0 +1,8 @@
+// apps/web/app/(purchasing)/compras/page.tsx
+export const revalidate = 0;
+
+import ComprasClient from './compras-client';
+
+export default function Page() {
+  return <ComprasClient />;
+}
